b6d21a56882d61a3efb7a2a3275b5c6ba141543e
spsandwichman <73349893+spsandwichman@users.noreply.github.com>
2023-11-26 18:15:20 -0500

    smol typo

--- src/arena.h
+++ src/arena.h
@@ -14,7 +14,7 @@ void* arena_alloc(arena* a, size_t size, size_t align);
 void  arena_free(arena* a);
 void  arena_destroy(arena* a);
 
-size_t align_forward size_t ptr, size_t align);
+size_t align_forward(size_t ptr, size_t align);
 size_t align_backward(size_t ptr, size_t align);
 
 #define is_pow_2(i) ((i & (i-1)) == 0)
